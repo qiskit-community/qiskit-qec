b6fc5b811150d61d4691d7ee398d5640cf327e45
Grace Harper <74260313+grace-harper-ibm@users.noreply.github.com>
2021-11-04 01:09:51 -0400

    init setup
    init setup

---
+++ qiskit_correct/illicit_affairs.py
@@ -0,0 +1,45 @@
+def lyrics():
+    print(
+        """
+          Make sure nobody sees you leave
+Hood over your head, keep your eyes down
+Tell your friends you're out for a run
+You'll be flushed when you return
+Take the road less traveled by
+Tell yourself you can always stop
+What started in beautiful rooms
+Ends with meetings in parking lots
+And that's the thing about illicit affairs
+And clandestine meetings and longing stares
+It's born from just one single glance
+But it dies, and it dies, and it dies
+A million little times
+Leave the perfume on the shelf
+That you picked out just for him
+So you leave no trace behind
+Like you don't even exist
+Take the words for what they are
+A dwindling, mercurial high
+A drug that only worked
+The first few hundred times
+And that's the thing about illicit affairs
+And clandestine meetings and stolen stares
+They show their truth one single time
+But they lie, and they lie, and they lie
+A million little times
+And you wanna scream
+Don't call me "kid"
+Don't call me "baby"
+Look at this godforsaken mess that you made me
+You showed me colors
+You know I can't see with anyone else
+Don't call me "kid"
+Don't call me "baby"
+Look at this idiotic fool that you made me
+You taught me a secret language
+I can't speak with anyone else
+And you know damn well
+For you I would ruin myself
+A million little times
+"""
+    )
---
+++ setup.py
@@ -0,0 +1,9 @@
+# currently used for backwards compatibility and to allow for editable code for development mode
+import setuptools
+
+setuptools.setup()
+
+
+# run pip install --editable .
+# https://setuptools.pypa.io/en/latest/userguide/development_mode.html
+# https://setuptools.pypa.io/en/latest/userguide/quickstart.html
