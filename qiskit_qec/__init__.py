63910d79a6f331bddc271feaf076dec7efac86e0
Grace Harper <74260313+grace-harper-ibm@users.noreply.github.com>
2021-11-04 03:26:46 -0400

    update init w docstring

--- qiskit_qec/__init__.py
+++ qiskit_qec/__init__.py
@@ -9,5 +9,5 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-
+"""Qiskit QEC"""
 __version__ = "0.0.1"
