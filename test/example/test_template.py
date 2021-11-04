812d8260c6c26f0ebb9650e037530598de6c9bb1
Grace Harper <74260313+grace-harper-ibm@users.noreply.github.com>
2021-11-04 03:52:53 -0400

    cov

--- test/example/test_template.py
+++ test/example/test_template.py
@@ -1,9 +1,11 @@
 """Tests for template."""
 from unittest import TestCase
+import qiskit_qec.heat_waves as hw
 
 
 class TestWillBeHere(TestCase):
     """Tests will be here."""
+
     def test_template_class(self):
         """Tests will start here."""
-        self.assertTrue(True)
+        hw.lyrics()
