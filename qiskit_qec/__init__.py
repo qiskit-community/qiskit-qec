14ce3641ee69d136514cb3120f19524e7eaaa23c
Grace Harper <74260313+grace-harper-ibm@users.noreply.github.com>
2021-11-04 03:24:45 -0400

    update init w docstring

--- qiskit_qec/__init__.py
+++ qiskit_qec/__init__.py
@@ -1,3 +1,13 @@
-# __init__.py
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 __version__ = "0.0.1"
