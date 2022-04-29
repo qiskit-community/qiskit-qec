a6f130c355843d4275802a7e46632ea4f454e766
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2022-04-29 16:14:22 -0400

    Issue-181 | Documentation: fix tutorials

--- qiskit_qec/qec_codebase/qec_codebase_interactor.py
+++ qiskit_qec/qec_codebase/qec_codebase_interactor.py
@@ -14,6 +14,7 @@
 
 import copy
 import json
+import logging
 import os
 import shutil
 import uuid
@@ -415,7 +416,7 @@ class QECCodeBase:
                 with open(path, "r", encoding="utf-8") as code_file:  # should except if invalid
                     qec_code_jsons = {**json.load(code_file), **qec_code_jsons}
             except FileNotFoundError:
-                print(f"{path} does not exist")  # TODO turn into logging
+                pass # print(f"{path} does not exist")  # TODO turn into logging
         if allow_cache:
             if (n_len, k_dim) in self.playground_cache:
                 qec_code_jsons = {**qec_code_jsons, **self.playground_cache[(n_len, k_dim)]}
