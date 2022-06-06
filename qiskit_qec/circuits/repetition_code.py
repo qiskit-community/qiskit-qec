1ace0955c60e761d0b2fa4bcfc3e7f8514394b5e
Grace Harper <grace.harper@ibm.com>
2022-06-06 16:21:54 -0400

    linted

--- qiskit_qec/circuits/repetition_code.py
+++ qiskit_qec/circuits/repetition_code.py
@@ -345,30 +345,3 @@ class RepetitionCodeCircuit:
         final_outcomes = [int(c) for c in finals]
 
         return x_gauge_outcomes, z_gauge_outcomes, final_outcomes
-
-    def partition_outcomes(
-        self, round_schedule: str, outcome: List[int]
-    ) -> Tuple[List[List[int]], List[List[int]], List[int]]:
-        """Extract measurement outcomes."""
-        # split into gauge and final outcomes
-        outcome = "".join([str(c) for c in outcome])
-        outcome = outcome.split(" ")
-        gs = outcome[0:-1]
-        gauge_outcomes = [[int(c) for c in r] for r in gs]
-        finals = outcome[-1]
-        # if circuit did not use resets, construct standard output
-        if not self.resets:
-            for i, layer in enumerate(gauge_outcomes):
-                for j, gauge_op in enumerate(layer):
-                    if i > 0:
-                        gauge_outcomes[i][j] = (gauge_op + gauge_outcomes[i - 1][j]) % 2
-        # assign outcomes to the correct gauge ops
-        if round_schedule == "z":
-            x_gauge_outcomes = []
-            z_gauge_outcomes = gauge_outcomes
-        else:
-            x_gauge_outcomes = gauge_outcomes
-            z_gauge_outcomes = []
-        final_outcomes = [int(c) for c in finals]
-
-        return x_gauge_outcomes, z_gauge_outcomes, final_outcomes
