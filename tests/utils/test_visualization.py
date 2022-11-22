e774d59278160db8332d62dd99ed4e94b986caf9
decodoku <jwo@zurich.ibm.com>
2022-11-22 14:01:49 +0100

    fix assertations

--- tests/utils/test_visualization.py
+++ tests/utils/test_visualization.py
@@ -14,15 +14,15 @@ class TestVisualization(TestCase):
         screen = Screen((400, 400), False, pixel_width=10)
         screen.pixel[9, 0].set_color("red")
         screen.pixel[0, 9].set_text("hello")
-        self.assertEqual(
+        self.assertTrue(
             screen.pixel[0, 0].button.button_style == "",
             "Default color not correct for pixel on screen.",
         )
-        self.assertEqual(
+        self.assertTrue(
             screen.pixel[9, 0].button.button_style == "danger",
             "Pixel on screen did not turn red when required.",
         )
-        self.assertEqual(
+        self.assertTrue(
             screen.pixel[0, 9].button.description == "hello",
             "Pixel on screen not displaying correct text",
         )
@@ -38,15 +38,15 @@ class TestVisualization(TestCase):
             pass
 
         engine = QiskitGameEngine(start, next_frame, size=8)
-        self.assertEqual(
+        self.assertTrue(
             engine.screen.pixel[0, 0].button.button_style == "",
             "Default color not correct for pixel in game engine.",
         )
-        self.assertEqual(
+        self.assertTrue(
             engine.screen.pixel[engine.size - 1, 0].button.button_style == "danger",
             "Pixel in game engine did not turn red when required.",
         )
-        self.assertEqual(
+        self.assertTrue(
             engine.screen.pixel[0, engine.size - 1].button.description == "hello",
             "Pixel in game engine not displaying correct text",
         )
