e48529bd26677a4a44f5795d06c87b7f583a5fed
Drew Vandeth <57962926+dsvandet@users.noreply.github.com>
2023-02-22 21:58:07 -0500

    Surface code circuit pass through (#325)
    * some css variables
    
    * minor improvements to repetition code
    
    * css attributes and partition_outcomes
    
    * remove partition_outcomes and add tests
    
    * lint and black
    
    * add check_nodes
    
    * fix distance calculation
    
    * fix check_nodes for marginal cases
    
    * fix thing
    
    * Decoders: Add Union Find decoder and fix bugs in Decoding Graph (#317)
    
    * DecodingGraph: Fix bugs in construction
    
    This patch solves two bugs:
    1) Doesn't add empty edges, which allowed the decoder to take "shortcuts".
    2) Used to treat edges with boundary as measurement errors, now it understands the difference.
    
    * Decoders: Add unoptimised UnionFind Decoder based on arXiv:1709.06218v3
    
    This patch adds an unoptimised version (without the UnionFind datastructure from the paper) of the UnionFind decoder from arXiv:1709.06218v3.
    
    Maybe it might be worthwhile to think about extracting the peeling method to a separate decoder.
    
    * UnionFindDecoder: Implement decoder like in paper
    
    This patch changes the implementation of the UnionFindDecoder from a more or less adhoc to the one described in the paper.
    
    * UnionFindDecoder: Actually test for correct logical outcome and fix bug
    
    This patch fixes a bug when highlighting nodes for logical 1.
    It also fixes testing, so that the logical outcome now also gets tested, not just the syndromes.
    
    * UnionFindDecoder: Make it run on repetition codes
    
    This patch removes the restriction that the code has to be a surface code, such that it can now also run on repetition codes.
    It also adds the required tests.
    
    * UnionFindDecoder: Fix bug when updating odd clusters
    
    This patch fixes a bug caused by not checking if a cluster becomes odd and not adding it back to the odd clusters.
    Also fixes repetition code test.
    
    * UnionFindDecoder: Generalize odd logic
    
    This patch generalizes the is_odd logic, it does that by keeping track of the non_typical nodes in each cluster and then computes is_odd when merging.
    Remembering it for each cluster is therefore not necessary anymore.
    This allows us to generalise the decoder to codes, where the assumption that a cluster is even when the number of atypical qubits is even doesn't hold.
    
    * UnionFindDecoder: Add support for ARCs and fix some implementation issues
    
    * Codes+UnionFindDecoder: Add method to code for odd check
    
    This patch adds methods to the codes that determines whether a given set of atypical syndrome nodes could have been caused by one or more errors.
    It then uses this method in the union find decoder.
    
    * UnionFindDecoder: Add method descriptions
    
    * UnionFindDecoder: Create cluster method and add support for multiple errors/clusters
    
    This patch adds the cluster method, which returns a list of lists of indices of the nodes inside the clusters.
    It also adds the ability to correct for multiple errors at  once, by adding support for multiple clusters.
    
    * SurfaceCode+RepCode+ARC: Rename is_cluster_even to is_cluster_neutral
    
    * Decoders: Move UnionFindDecoder to hdrg_decoders.py, rename ClusteringDecoder to BravyiHaahDecoder and create generic base class for clustering decoders
    
    * UnionFindDecoder: Fix some bugs in the decoder and add a test that measures logical error rate
    
    * Testing: Fix error rate testing for the union find decoder
    
    * Tutorials: Create tiny tutorial explaining how to use union find
    
    * black
    
    * more black
    
    * restore exception for pylint
    
    * complete move of rep tests
    
    * fix some linting
    
    * Fixed type hint error
    
    * UnionFindDecoder+Tests: Fix lint issues (#326)
    
    Co-authored-by: James Wootton <jwo@zurich.ibm.com>
    
    * more linting
    
    ---------
    
    Co-authored-by: decodoku <jwo@zurich.ibm.com>
    Co-authored-by: James Wootton <decodoku@gmail.com>
    Co-authored-by: Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>

--- src/qiskit_qec/decoders/hdrg_decoders.py
+++ src/qiskit_qec/decoders/hdrg_decoders.py
@@ -16,15 +16,34 @@
 
 """Hard decision renormalization group decoders."""
 
-from rustworkx import distance_matrix
-from rustworkx import connected_components
+from copy import copy, deepcopy
+from dataclasses import dataclass
+from typing import Dict, List, Set
+from rustworkx import connected_components, distance_matrix, PyGraph
 
-from qiskit_qec.decoders.decoding_graph import DecodingGraph
 from qiskit_qec.circuits.repetition_code import ArcCircuit, RepetitionCodeCircuit
+from qiskit_qec.decoders.decoding_graph import DecodingGraph
 from qiskit_qec.exceptions import QiskitQECError
 
 
 class ClusteringDecoder:
+    """
+    Generic base class for clustering decoders.
+    """
+
+    def __init__(
+        self,
+        code_circuit,
+        decoding_graph: DecodingGraph = None,
+    ):
+        self.code = code_circuit
+        if decoding_graph:
+            self.decoding_graph = decoding_graph
+        else:
+            self.decoding_graph = DecodingGraph(self.code)
+
+
+class BravyiHaahDecoder(ClusteringDecoder):
     """Decoder based on finding connected components within the decoding graph."""
 
     def __init__(
@@ -35,11 +54,8 @@ class ClusteringDecoder:
         if not isinstance(code_circuit, (ArcCircuit, RepetitionCodeCircuit)):
             raise QiskitQECError("Error: code_circuit not supported.")
 
-        self.code = code_circuit
-        if decoding_graph:
-            self.decoding_graph = decoding_graph
-        else:
-            self.decoding_graph = DecodingGraph(self.code)
+        super().__init__(code_circuit, decoding_graph)
+
         if isinstance(self.code, ArcCircuit):
             self.z_logicals = self.code.z_logicals
         elif isinstance(self.code, RepetitionCodeCircuit):
@@ -210,3 +226,307 @@ class ClusteringDecoder:
             corrected_z_logicals.append(corrected_logical)
 
         return corrected_z_logicals
+
+
+@dataclass
+class SpanningForest:
+    """
+    Spanning forest for the peeling decoder.
+    """
+
+    vertices: Dict[int, List[int]]
+    edges: List[int]
+
+
+@dataclass
+class BoundaryEdge:
+    """
+    Boundary edge for the boundary of a UnionFindDecoderCluster.
+    """
+
+    index: int
+    cluster_vertex: int
+    neighbour_vertex: int
+    data: Dict[str, object]
+
+    def reverse(self):
+        """
+        Returns a reversed version of the boundary edge (cluster and neighbour vertex flipped)
+        """
+        return BoundaryEdge(
+            index=self.index,
+            cluster_vertex=self.neighbour_vertex,
+            neighbour_vertex=self.cluster_vertex,
+            data=self.data,
+        )
+
+
+@dataclass
+class UnionFindDecoderCluster:
+    """
+    Cluster for the UnionFindDecoder
+    """
+
+    boundary: List[BoundaryEdge]
+    atypical_nodes: Set[int]
+    fully_grown_edges: Set[int]
+    size: int
+
+
+@dataclass
+class FusionEntry:
+    """
+    Entry for the fusion list between the growing and merging of the union find decoder.
+    """
+
+    u: int
+    v: int
+    connecting_edge: BoundaryEdge
+
+
+class UnionFindDecoder(ClusteringDecoder):
+    """
+    Decoder based on growing clusters around syndrome errors to
+    "convert" them into erasure errors, which can be corrected easily,
+    by the peeling decoder in case of the surface code, or by checking for
+    interference with the boundary in case of an abritrary ARC.
+
+    TODO: Add weights to edges of graph according to Huang et al (see. arXiv:2004.04693, section III)
+
+    See arXiv:1709.06218v3 for more details.
+    """
+
+    def __init__(
+        self,
+        code,
+        logical: str,
+        decoding_graph: DecodingGraph = None,
+    ) -> None:
+        super().__init__(code, decoding_graph)
+        self.logical = logical
+        self.graph = deepcopy(self.decoding_graph.graph)
+        self.clusters: List[List[int]] = []
+        self.odd_cluster_roots: Set[int] = []
+
+    def process(self, string: str):
+        """
+        Process an output string and return corrected final outcomes.
+
+        Args:
+            string (str): Output string of the code.
+        Returns:
+            corrected_z_logicals (list): A list of integers that are 0 or 1.
+        These are the corrected values of the final transversal
+        measurement, corresponding to the logical operators of
+        self.z_logicals.
+        """
+        self.graph = deepcopy(self.decoding_graph.graph)
+        string = "".join([str(c) for c in string[::-1]])
+        output = [int(bit) for bit in list(string.split(" ", maxsplit=self.code.d)[0])][::-1]
+        highlighted_nodes = self.code.string2nodes(string, logical=self.logical)
+        if not highlighted_nodes:
+            return output  # There's nothing for us to do here
+        clusters = self.cluster(highlighted_nodes)
+
+        for cluster in clusters:
+            erasure = self.graph.subgraph(cluster)
+            if isinstance(self.code, ArcCircuit):
+                # NOTE: it just corrects for final logical readout
+                for node in erasure.nodes():
+                    if node["is_boundary"]:
+                        # FIXME: Find a general way to go from physical qubit
+                        # index to code qubit index
+                        qubit_to_be_corrected = int(node["qubits"][0] / 2)
+                        output[qubit_to_be_corrected] = (output[qubit_to_be_corrected] + 1) % 2
+                continue
+
+            flipped_qubits = self.peeling(erasure)
+            for qubit_to_be_corrected in flipped_qubits:
+                output[qubit_to_be_corrected] = (output[qubit_to_be_corrected] + 1) % 2
+
+        return output
+
+    def cluster(self, nodes) -> List[List[int]]:
+        """
+        Create clusters using the union-find algorithm.
+
+        Args:
+            nodes (List): List of non-typical nodes in the syndrome graph,
+            of the type produced by `string2nodes`.
+
+        Returns:
+            FIXME: Make this more expressive.
+            clusters (List[List[int]]): List of Lists of indices of nodes in clusters
+        """
+        node_indices = [self.graph.nodes().index(node) for node in nodes]
+        for node_index, _ in enumerate(self.graph.nodes()):
+            self.graph[node_index]["syndrome"] = node_index in node_indices
+            self.graph[node_index]["root"] = node_index
+
+        for edge in self.graph.edges():
+            edge["growth"] = 0
+            edge["fully_grown"] = False
+
+        self.clusters: Dict[int, UnionFindDecoderCluster] = {}
+        self.odd_cluster_roots = set(node_indices)
+        for node_index in self.graph.node_indices():
+            boundary_edges = []
+            for edge_index, (_, neighbour, data) in dict(
+                self.graph.incident_edge_index_map(node_index)
+            ).items():
+                boundary_edges.append(BoundaryEdge(edge_index, node_index, neighbour, data))
+            self.clusters[node_index] = UnionFindDecoderCluster(
+                boundary=boundary_edges,
+                fully_grown_edges=set(),
+                atypical_nodes=set([node_index])
+                if node_index in self.odd_cluster_roots
+                else set([]),
+                size=1,
+            )
+
+        while self.odd_cluster_roots:
+            fusion_edge_list = self._grow_clusters()
+            self._merge_clusters(fusion_edge_list)
+
+        cluster_nodes = []
+        for _, cluster in self.clusters.items():
+            if not cluster.atypical_nodes:
+                continue
+            nodes = set()
+            for edge in cluster.fully_grown_edges:
+                nodes |= set(self.graph.get_edge_endpoints_by_index(edge))
+            cluster_nodes.append(list(nodes))
+        return cluster_nodes
+
+    def find(self, u: int) -> int:
+        """
+        Find() function as described in the paper that returns the root
+        of the cluster of a node, including path compression.
+
+        Args:
+            u (int): The index of the node in the decoding graph.
+
+        Returns:
+            root (int): The root of the cluster of node u.
+        """
+        if self.graph[u]["root"] == u:
+            return self.graph[u]["root"]
+
+        self.graph[u]["root"] = self.find(self.graph[u]["root"])
+        return self.graph[u]["root"]
+
+    def _grow_clusters(self) -> List[FusionEntry]:
+        """
+        Grow every "odd" cluster by half an edge.
+
+        Returns:
+            fusion_edge_list (List[FusionEntry]): List of edges that connect two
+            clusters that will be merged in the next step.
+        """
+        fusion_edge_list: List[FusionEntry] = []
+        for root in self.odd_cluster_roots:
+            cluster = self.clusters[root]
+            for edge in cluster.boundary:
+                edge.data["growth"] += 0.5
+                if edge.data["growth"] >= edge.data["weight"] and not edge.data["fully_grown"]:
+                    edge.data["fully_grown"] = True
+                    cluster.fully_grown_edges.add(edge.index)
+                    fusion_entry = FusionEntry(
+                        u=edge.cluster_vertex, v=edge.neighbour_vertex, connecting_edge=edge
+                    )
+                    fusion_edge_list.append(fusion_entry)
+        return fusion_edge_list
+
+    def _merge_clusters(self, fusion_edge_list: List[FusionEntry]) -> None:
+        """
+        Merges the clusters based on the fusion_edge_list computed in _grow_clusters().
+        Updates the odd_clusters list by recomputing the neutrality of the newly merged clusters.
+
+        Args:
+            fusion_edge_list (List[FusionEntry]): List of edges that connect two
+            clusters that was computed in _grow_clusters().
+        """
+        for entry in fusion_edge_list:
+            root_u, root_v = self.find(entry.u), self.find(entry.v)
+            if root_u == root_v:
+                continue
+            new_root = root_v if self.clusters[root_v].size > self.clusters[root_u].size else root_u
+            root_to_update = root_v if new_root == root_u else root_u
+
+            cluster = self.clusters[new_root]
+            other_cluster = self.clusters.pop(root_to_update)
+
+            # Merge boundaries
+            cluster.boundary += other_cluster.boundary
+            cluster.boundary.remove(entry.connecting_edge)
+            cluster.boundary.remove(entry.connecting_edge.reverse())
+
+            cluster.atypical_nodes |= other_cluster.atypical_nodes
+            cluster.fully_grown_edges |= other_cluster.fully_grown_edges
+            cluster.size += other_cluster.size
+
+            # update odd_cluster_roots
+            if not self.code.is_cluster_neutral(
+                [self.graph[node] for node in cluster.atypical_nodes]
+            ):
+                self.odd_cluster_roots.add(new_root)
+            else:
+                self.odd_cluster_roots.discard(new_root)
+            self.odd_cluster_roots.discard(root_to_update)
+            self.graph[root_to_update]["root"] = new_root
+
+    def peeling(self, erasure: PyGraph) -> List[int]:
+        """ "
+        Runs the peeling decoder on the erasure provided.
+        Assumes that the erasure is one connected component, if not it will run in an
+        infinite loop in the tree construction.
+        It works by first producing a spanning forest of the erasure and then
+        going backwards through the edges of the tree computing the error based on the syndrome.
+        Based on arXiv:1703.01517.
+
+        TODO: Extract to a separate decoder.
+
+        Args:
+            erasure (PyGraph): subgraph of the syndrome graph that represents the erasure.
+
+        Returns:
+            errors (List[int]): List of qubit indices on which Pauli errors occurred.
+        """
+        tree = SpanningForest(vertices={}, edges=[])
+
+        # Construct spanning forest
+        # Pick starting vertex
+        for vertex in erasure.node_indices():
+            if erasure[vertex]["is_boundary"]:
+                tree.vertices[vertex] = []
+                break
+        if not tree.vertices:
+            tree.vertices[erasure.node_indices()[0]] = []
+
+        # Expand forest |V| - 1 times, constructing it
+        while len(tree.edges) < len(erasure.nodes()) - 1:
+            vertices = copy(tree.vertices)
+            for node in vertices.keys():
+                if len(tree.edges) >= len(erasure.nodes()) - 1:
+                    break
+                for edge, (_, neighbour, _) in dict(erasure.incident_edge_index_map(node)).items():
+                    if not neighbour in tree.vertices.keys():
+                        tree.edges.append(edge)
+                        tree.vertices[neighbour] = []
+                        tree.vertices[node].append(edge)
+                        break
+
+        edges = set()
+        for edge in tree.edges[::-1]:
+            endpoints = erasure.get_edge_endpoints_by_index(edge)
+            pendant_vertex = endpoints[0] if not tree.vertices[endpoints[0]] else endpoints[1]
+            tree_vertex = endpoints[0] if pendant_vertex == endpoints[1] else endpoints[1]
+            tree.vertices[tree_vertex].remove(edge)
+            if erasure[pendant_vertex]["syndrome"] and not erasure[pendant_vertex]["is_boundary"]:
+                edges.add(edge)
+                erasure[tree_vertex]["syndrome"] = not erasure[tree_vertex]["syndrome"]
+                erasure[pendant_vertex]["syndrome"] = False
+
+        return [
+            erasure.edges()[edge]["qubits"][0] for edge in edges if erasure.edges()[edge]["qubits"]
+        ]
