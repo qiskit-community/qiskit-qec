1cfce65130d783c75e8d051b6887c24a274d6f52
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2023-03-09 16:28:54 +0100

    Update decoding graph caching to support new node tzpes

--- src/qiskit_qec/decoders/temp_graph_util.py
+++ src/qiskit_qec/decoders/temp_graph_util.py
@@ -4,6 +4,8 @@ import os
 import networkx as nx
 import rustworkx as rx
 
+from qiskit_qec.utils.decoding_graph_attributes import DecodingGraphEdge, DecodingGraphNode
+
 
 def ret2net(graph: rx.PyGraph):
     """Convert rustworkx graph to equivalent networkx graph."""
@@ -36,7 +38,7 @@ def write_graph_to_json(graph: rx.PyGraph, filename: str):
     fp.close()
 
 
-def get_cached_graph(path):
+def get_cached_decoding_graph(path):
     """
     Returns graph cached in file at path "file" using cache_graph method.
     """
@@ -45,13 +47,28 @@ def get_cached_graph(path):
             json_data = json.loads(file.read())
             net_graph = nx.node_link_graph(json_data)
         ret_graph = rx.networkx_converter(net_graph, keep_attributes=True)
-        for node in ret_graph.nodes():
+        for node_index, node in zip(ret_graph.node_indices(), ret_graph.nodes()):
             del node["__networkx_node__"]
+            qubits = node.pop("qubits")
+            time = node.pop("time")
+            index = node.pop("index")
+            is_boundary = node.pop("is_boundary")
+            properties = node.copy()
+            node = DecodingGraphNode(is_boundary=is_boundary, time=time, index=index, qubits=qubits)
+            node.properties = properties
+            ret_graph[node_index] = node
+        for edge_index, edge in zip(ret_graph.edge_indices(), ret_graph.edges()):
+            weight = edge.pop("weight")
+            qubits = edge.pop("qubits")
+            properties = edge.copy()
+            edge = DecodingGraphEdge(weight=weight, qubits=qubits)
+            edge.properties = properties
+            ret_graph.update_edge_by_index(edge_index, edge)
         return ret_graph
     return None
 
 
-def cache_graph(graph, path):
+def cache_decoding_graph(graph, path):
     """
     Cache rustworkx PyGraph to file at path.
     """
