a0c073da4a165dca4bc18a5defb45ba8e45bb4a7
Andrew Cross <awcross@us.ibm.com>
2023-03-15 22:28:41 -0400

    approximate edge flip probabilities saturate at 1/2.
    Rather than raise an exception when the approximations appear to
    break down, instead choose the maximum edge flip probability to
    be 1/2.

--- src/qiskit_qec/decoders/circuit_matching_decoder.py
+++ src/qiskit_qec/decoders/circuit_matching_decoder.py
@@ -346,7 +346,9 @@ class CircuitModelMatchingDecoder(ABC):
                     )
                     restriction = {x: assignment[x] for x in edge_data["weight_poly"].gens}
                     p = edge_data["weight_poly"].eval(restriction).evalf()
-                    assert p < 0.5, "edge flip probability too large"
+                    # if approximate edge flip probability is large, saturate at 1/2
+                    if p > 0.5:
+                        p = 0.5
                     edge_data["weight"] = log((1 - p) / p)
         self.matcher.preprocess(self.graph)
 
