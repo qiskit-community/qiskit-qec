51760cafa318090dc69d6c73bac7755176b5e3e0
James Wootton <jwo@zurich.ibm.com>
2023-06-09 16:04:25 +0200

    339 integrate stim (#359)
    * Code circuit builder -> integrate stim (#340)
    
    * create CodeCircuit class (#329) (#332)
    
    * create CodeCircuit class
    
    * add more detail to init
    
    * add default is_cluster_neutral
    
    * add css code circuit
    
    * Adding two methods: 'to_stim_circuit' and 'get_counts_via_stim' (#1)
    
    * Update css_code.py
    
    * Update css_code.py
    
    * Update css_code.py
    
    * stim integration in the CssCodeCircuit class (#341)
    
    * Update css_code.py
    
    * Update css_code.py
    
    * Update css_code.py
    
    * add stim
    
    * separate stim functions from class
    
    * move stim tools to utils
    
    * black and lint
    
    * document stim tools
    
    * document stim tools
    
    * add general noisification of circuits
    
    * add support for StabSubsystemCode
    
    * conflict avoidance
    
    * Update __init__.py
    
    * fix conflict
    
    * complete StabSubsystem compatibility
    
    * Stim DecodingGraph (#356)
    
    * decoding graph via stim
    
    New methods: 
    1) 'stim_circuit_with_detectors' converts the qiskit circuit to stim and appends detectors
    2) '_make_syndrome_graph' creates a rustworkx graph and hyper edge list to be passed onto the DecodingGraph class
    
    * qiskit style decoding graph from stim circuit
    
    New function: 'detector_error_model_to_rx_graph' creates a qiskit style decoding graph from a stim circuit
    
    * Update decoding_graph_attributes.py
    
    * undo conflict fix
    
    * undo conflict resolution (#358)
    
    * Test (for CSS codes), black, lint, py39 tests (#361)
    
    * black + lint
    
    * Add files via upload
    
    * Update stim_tools.py
    
    * Update css_code.py
    
    * Grace's change requests (#363)
    
    1) stim repeat block is still needed
    2) specify what noise model means
    
    * per review
    
    * Documentation and functions (#374)
    
    * fix linting issues
    
    ---------
    
    Co-authored-by: hetenyib <55080156+hetenyib@users.noreply.github.com>

---
+++ test/code_circuits/test_css_codes_with_stim.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test for the CSSCodeCircuit class for Heavy-HEX code with pymatching"""
+import unittest
+import pymatching
+
+from qiskit_qec.codes.hhc import HHC
+from qiskit_qec.circuits.css_code import CSSCodeCircuit
+from qiskit_qec.decoders.decoding_graph import DecodingGraph
+
+
+class TestCircuitMatcher(unittest.TestCase):
+    """Test for the CSSCodeCircuit class for Heavy-HEX code with pymatching"""
+
+    def log_failure_dists(self, error_rate: float):
+        """Constructs the stim circuit and the decoding graph (via a stim DetectorErrorModel)
+        for the heavy-hex code and tests it on 10_000 samples for distance 3 and 5.
+        Returns the logical failure for distance 3 and 5 at the specified error rate.
+        Below ~100_000 shotss, the runtime is limited by the decoding graph construction,
+        not the sampling."""
+        dist_list = [3, 5]
+        num_shots = 10_000
+        log_fail_d = []
+        for d in dist_list:
+            code = HHC(d)
+            css_code = CSSCodeCircuit(code, T=d, basis="x", noise_model=(error_rate, error_rate))
+            graph = DecodingGraph(css_code).graph
+            m = pymatching.Matching(graph)
+            stim_circuit = css_code.stim_circuit_with_detectors()["0"]
+            stim_sampler = stim_circuit.compile_detector_sampler()
+            num_correct = 0
+            stim_samples = stim_sampler.sample(num_shots, append_observables=True)
+            for sample in stim_samples:
+                actual_observable = sample[-1]
+                detectors_only = sample.copy()
+                detectors_only[-1] = 0
+                predicted_observable = m.decode(detectors_only)[0]
+                num_correct += actual_observable == predicted_observable
+            log_fail_d.append((num_shots - num_correct) / num_shots)
+        return log_fail_d
+
+    def test_HHC(self):
+        """Tests the order of logical failure rates for two distances.
+        One test is below threshold, one is above. (Threshold ~3.5% for the test code)
+        Runtime is approx 5 seconds."""
+        error_rate = 0.01  # this should be below threshold
+        log_fail_dist = self.log_failure_dists(error_rate)
+        self.assertTrue(log_fail_dist[0] > 2 * log_fail_dist[1])
+
+        error_rate = 0.1  # this should be above threshold
+        log_fail_dist = self.log_failure_dists(error_rate)
+        self.assertTrue(log_fail_dist[0] < log_fail_dist[1])
+
+
+if __name__ == "__main__":
+    unittest.main()
