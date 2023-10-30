a3a4b8dc8cd3c3ffca3c47765ddce79c7894ba2a
Bence Hetényi <55080156+hetenyib@users.noreply.github.com>
2023-10-30 09:53:05 +0100

    Update css_code.py
    linting

--- src/qiskit_qec/circuits/css_code.py
+++ src/qiskit_qec/circuits/css_code.py
@@ -128,6 +128,7 @@ class CSSCodeCircuit(CodeCircuit):
                     if set(stabilizer).intersection(set(gauge)) == set(gauge):
                         gauges.append(g)
                 self._gauges4stabilizers[j].append(gauges)
+    self.detectors, self.logicals = self.stim_detectors()
 
     def _get_code_properties(self):
         if isinstance(self.code, StabSubSystemCode):
@@ -256,7 +257,6 @@ class CSSCodeCircuit(CodeCircuit):
         Returns:
             a list of 'DecodingGraphNode()'-s corresponding to the triggered detectors
         """
-        self.detectors, self.logicals = self.stim_detectors()
 
         nodes = string2nodes_with_detectors(
             string=string,
